f901a86fcb60b42983488e2c9e56832553faeba2
yaraaamohsen <0yaramohsen00@gmail.com>
2025-03-09 23:14:02 +0200

    Create Action - View (Create)

--- Company.Owner.PL/Controllers/DepartmentController.cs
+++ Company.Owner.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.Owner.BLL.Interfaces;
 using Company.Owner.BLL.Reposatories;
+using Company.Owner.DAL.Models;
+using Company.Owner.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Owner.PL.Controllers
@@ -23,7 +25,32 @@ namespace Company.Owner.PL.Controllers
             return View(departments); // Sent department As A Model
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) // Server Side Validation
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt,
+                };
 
+                var count = _departmentRepository.Add(department);
+                if(count > 0)
+                {
+                    return RedirectToAction(nameof(Index ));
+                }
+            }
+            return View();
+
+        }
     }
 }
---
+++ Company.Owner.PL/Dtos/CreateDepartmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Owner.PL.Dtos
+{
+    public class CreateDepartmentDto
+    {
+        [Required (ErrorMessage ="Code Is Required !")]
+        public int Code { get; set; }
+
+        [Required(ErrorMessage = "Name Is Required !")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Date Is Required !")]
+        public DateTime CreateAt { get; set; }
+    }
+}
