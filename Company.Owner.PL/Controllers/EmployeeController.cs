54a23774d191a5bfe737339c740586c4f99bfd17
yaraaamohsen <0yaramohsen00@gmail.com>
2025-03-12 07:10:26 +0200

    implementation For Employee Index (Action, view)

---
+++ Company.Owner.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Company.Owner.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Owner.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRemository _employeeRepository;
+
+        public EmployeeController(IEmployeeRemository employeeRepository)
+        {
+            _employeeRepository = employeeRepository; 
+        }
+        public IActionResult Index()
+        {
+            var employees = _employeeRepository.GetAll();
+            return View(employees);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
