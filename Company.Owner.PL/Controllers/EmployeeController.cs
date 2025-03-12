3696590c1a4d2c3936b5ad86ba9a27ce232b7471
yaraaamohsen <0yaramohsen00@gmail.com>
2025-03-12 22:06:03 +0200

    Implementation Of Employee.Create (View- Actions)

--- Company.Owner.PL/Controllers/EmployeeController.cs
+++ Company.Owner.PL/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
 ﻿using Company.Owner.BLL.Interfaces;
+using Company.Owner.DAL.Models;
+using Company.Owner.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Owner.PL.Controllers
@@ -16,9 +18,31 @@ namespace Company.Owner.PL.Controllers
             var employees = _employeeRepository.GetAll();
             return View(employees);
         }
-
         public IActionResult Create()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateEmployeeDto model)
+        {
+            if(ModelState.IsValid)
+            {
+                var employee = new Employee()
+                {
+                    Name = model.Name,
+                    Age = model.Age,
+                    Address = model.Address,
+                    Salary = model.Salary,
+                    Email = model.Email,
+                    Phone = model.Phone,
+                    IsActive = model.IsActive,
+                    IsDeleted = model.IsDeleted,
+                    HiringDate = model.HiringDate,
+                    CreateAt = model.CreateAt
+                };
+                var count = _employeeRepository.Add(employee);
+                if(count > 0) return RedirectToAction(nameof(Index));
+            }
             return View();
         }
     }
---
+++ Company.Owner.PL/Dtos/CreateEmployeeDto.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Company.Owner.PL.Dtos
+{
+    public class CreateEmployeeDto
+    {
+        [Required(ErrorMessage = "Name Is Required !")]
+        public string Name { get; set; }
+
+        [Range(22,60, ErrorMessage = "Age Is Out Of Range, It Must Be Between 22 And 60")]
+        public int? Age { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Salary { get; set; }
+
+        [RegularExpression(@"^[1-9]{1,3}-[A-Za-z]{2,10}-[A-Za-z]{2,10}-[A-Za-z]{2,10}$", ErrorMessage ="Address must be 123-street-City-Region")]
+        public string Address { get; set; }
+
+        [RegularExpression(@"^\+20(10|11|12|15)\d{8}$", ErrorMessage = "Phone Number must be +201xxxxxxxxx")]
+        public string Phone { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email Is Not Valid")]
+        public string Email { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
+
+        [DisplayName("Hiring Date")]
+        public DateTime HiringDate { get; set; }
+
+        [DisplayName("Date Of Creation")]
+        public DateTime CreateAt { get; set; }
+    }
+}
