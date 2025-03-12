a9d6a19f6651d912eedd9f1ea511edc150acab68
yaraaamohsen <0yaramohsen00@gmail.com>
2025-03-12 05:53:50 +0200

    Add Employee Table DAL

---
+++ Company.Owner.DAL/Data/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Owner.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Company.Owner.DAL.Data.Configurations
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(E => E.Salary).HasColumnType("decimal(18,2)");
+        }
+    }
+}
